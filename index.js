31a688c6d56c1fa737f396956969089e8cf152e3
Marathe <darshan.marathe@philips.com>
2021-03-09 13:50:02 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const fs = require('fs');
+const express = require('express');
+var cors = require('cors')
+const app = express()
+const port = 3000
+
+
+app.use(cors())
+
+app.get('/:type/:id', async (req, res) => {
+    let item =await readfileContent(req.params);
+    res.send(item)
+})
+
+function readfileContent({type, id}) {
+    return new Promise((res, rej) => {
+        fs.readFile(`./data/${type}/${id}.json`, (err, data) => {
+            if (err) rej(err);
+            let record_data = JSON.parse(data);
+            res(record_data)
+        });
+
+    });
+}
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
