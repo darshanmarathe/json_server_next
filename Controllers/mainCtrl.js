a9b127ead32dbe936506e140e5002eb296a33bad
Marathe <darshan.marathe@philips.com>
2022-01-13 14:24:51 +0530

    Added Caching....

--- Controllers/mainCtrl.js
+++ Controllers/mainCtrl.js
@@ -1,12 +1,17 @@
 var repo = null;
-
-const Init = (_repo) => {
+var Cache = null
+const Init = (_repo , _cache) => {
   repo = _repo;
+  Cache = _cache;  
 }
 
 const Index = async (req, res) => {
-  
-  let listOfCollections = await repo.CollectionList();
+  let listOfCollections = Cache.CollectionList();
+  if(!listOfCollections){
+    listOfCollections = await repo.CollectionList();
+    Cache.Set('collectionList', listOfCollections);
+  } 
+   
   listOfCollections = listOfCollections.filter((x) => x.toLowerCase() !== "admin")
   let html = `
     <h1> Routes already available</h1>
@@ -21,16 +26,27 @@ const Index = async (req, res) => {
 
 const Get = async (req, res) => {
   if(req.params.type.toUpperCase() === 'FAVICON.ICO') 
-      {
+  {
         res.send(null);
       return;
     }
-
-  let items = await repo.GetData(req.params, req.query);
+  let items = await Cache.GetData(req.params, req.query)
+  console.log(items)
+  if(!items){
+    items = await repo.GetData(req.params, req.query);
+    const { type } = req.params;
+    Cache.Set(type, items); 
+  }  
+  
   res.send(items);
 }
 const GetById = async (req, res) => {
-  let item = await repo.GetDataById(req.params);
+  let item = await Cache.GetDataById(req.params);
+  if(!item){
+    item = await repo.GetDataById(req.params);
+    const { type, id } = req.params;
+    Cache.Set(`${type}_${id}`, item);
+  }
   if (typeof item === 'object')
     res.send(item);
   else
@@ -41,6 +57,7 @@ const Post = async (req, res) => {
   try {
     const item = await repo.Create(type, req.body);
     res.send(item);
+    Cache.Set(`${type}_${item._id}`, item);
   } catch (error) {
     res.send(error);
   }
@@ -50,6 +67,7 @@ const Put = async (req, res) => {
   if (req.body.id) delete req.body.id;
   try {
     let obj = await repo.Update(type, req.body, id);
+    Cache.Set(`${type}_${id}`, obj);
     res.send(obj);
   } catch (error) {
     res.send(error);
@@ -58,6 +76,9 @@ const Put = async (req, res) => {
 const Delete = async (req, res) => {
   let { type, id } = req.params;
   await repo.Delete(type, id);
+  Cache.Destroy(type)
+  Cache.Destroy(`${type}_${id}`)
+  
   res.send("Deleted.");
 }
 
--- run.bat
+++ run.bat
@@ -3,5 +3,5 @@ cls
 @echo starting server......
 set PROVIDER=postgres
 set CONNECTION_STRING=postgres://ykqeaogj:ZeEjD72l6u0rNoL0uCdrygBijRc_SpPH@rosie.db.elephantsql.com/ykqeaogj
-
+set CACHE_TTL=10000
 npm run watch
