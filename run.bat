ed1b84e6ce5e239fa53bc50a1da1ea7e022d8d4b
Darshan Marathe <darshanmarathe@gmail.com>
2022-01-14 18:10:33 +0530

    run file

--- run.bat
+++ run.bat
@@ -1,14 +1,12 @@
 @echo off
 cls
 @echo starting server......
-<<<<<<< HEAD
-set PROVIDER=sql
-set CONNECTION_STRING=Server^=tcp:minjsonserv.database.windows.net,1433;Initial Catalog^=JSONSERV;Persist Security Info^=False;User ID^=servsa;Password^=Admin@123;MultipleActiveResultSets^=False;Encrypt^=True;TrustServerCertificate^=False;Connection Timeout^=30;
-
-=======
 set PROVIDER=postgres
 set CONNECTION_STRING=postgres://ykqeaogj:ZeEjD72l6u0rNoL0uCdrygBijRc_SpPH@rosie.db.elephantsql.com/ykqeaogj
 set CACHE_TTL=100
->>>>>>> main
 npm run watch
+
+
+
+
 
