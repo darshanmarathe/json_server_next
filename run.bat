5b4914178af47e87a0161bbe3f98d775d91f2b01
darshanmarathe <darshanmarathe@gmail.com>
2022-01-30 17:37:53 +0530

    work in progress

--- run.bat
+++ run.bat
@@ -4,6 +4,7 @@ cls
 set PROVIDER=filesys
 set CONNECTION_STRING=postgres://ykqeaogj:ZeEjD72l6u0rNoL0uCdrygBijRc_SpPH@rosie.db.elephantsql.com/ykqeaogj
 set CACHE_TTL=100
+set PORT=4000
 npm run watchadm
 
 
